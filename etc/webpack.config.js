97e3753183e33c9e3eb4856d706abfc68a4ac99f
Mike Jay Stetsyshyn <jays.demons@gmail.com>
2016-06-03 23:44:15 +0300

    initial client-side

---
+++ etc/webpack.config.js
@@ -0,0 +1,25 @@
+﻿module.exports = {
+    entry: "./app/index.js",
+    output: {
+        path: __dirname,
+        filename: "./dist/app.compiled.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.jade$/, loaders: ["babel?presets[]=es2015", "jade"] },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower)/,
+                loader: "babel?presets[]=es2015"
+            }
+        ]
+    },
+    resolve: {
+        root: __dirname,
+        moduleDirectories: [__dirname + "/app"],
+        alias: {
+            "jquery$": __dirname + "/bower/jquery/dist/jquery.js",
+            "bootstrap": __dirname + "/bower/bootstrap/dist"
+        }
+    }
+};
