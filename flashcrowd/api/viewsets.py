7a6ea4c7c2847035a23e3f08f5ed14086493e547
Mike Jay Stetsyshsyn <jays.demons@gmail.com>
2016-06-04 21:50:22 +0300

    Fix for badges

--- flashcrowd/api/viewsets.py
+++ flashcrowd/api/viewsets.py
@@ -198,7 +198,7 @@ class UserBadgesViewSet(ModelViewSet):
         all_badges = Badge.objects.all()
         user_calls = Call.objects.filter(executor=user.id)
         user_tasks = Task.objects.filter(id__in=[call.task.id for call in user_calls])
-        exclude_badges_list = UserBadge.objects.filter(user=user.id)
+        exclude_badges_list = [ub.badge for ub in UserBadge.objects.filter(user=user.id)]
 
         for badge in all_badges:
             if badge not in exclude_badges_list:
