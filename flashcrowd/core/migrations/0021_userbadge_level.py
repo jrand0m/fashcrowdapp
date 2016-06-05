cdc4511998f185964be0acaf5454027defb6bf8c
Andrew Dunai <andunai@gmail.com>
2016-06-05 16:30:26 +0300

    Fix.

--- flashcrowd/core/migrations/0021_userbadge_level.py
+++ flashcrowd/core/migrations/0021_userbadge_level.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0019_auto_20160605_1151'),
+        ('core', '0020_auto_20160605_1205'),
     ]
 
     operations = [
