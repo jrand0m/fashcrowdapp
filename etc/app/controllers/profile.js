1260ed6e4338cadc3e76230d863db507661c692a
Mike Jay Stetsyshyn <jays.demons@gmail.com>
2016-06-05 15:33:09 +0300

    more UI

--- etc/app/controllers/profile.js
+++ etc/app/controllers/profile.js
@@ -3,14 +3,23 @@ import base, {route} from './base'
 import users from 'services/users'
 
 import Profile from 'templates/profile.jade'
+import Awards from 'templates/awards.jade'
 
 export default class extends base {
 
     @route('/profile')
-    index() {
+    profile() {
         this.freeze();
 
         users.me()
             .then(_ => this.push(Profile, _))
+    }
+
+    @route('/awards')
+    awards() {
+        this.freeze();
+
+        users.me()
+            .then(_ => this.push(Awards, _))
     }
 }
